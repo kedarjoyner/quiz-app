3c48ae915cb40dc63564de6cbbfbf40c1465529f
kedarjoyner <kedarjoyner@gmail.com>
2015-11-04 14:13:02 -0600

    light and dark working

--- js/quiz.js
+++ js/quiz.js
@@ -1,5 +1,40 @@
 
+var button = "";
+	var questionCurrent = 0;
+	var questionTotal = 10;
+
 $(document).ready(function() {
-	$(".main").hide();
-	
+
+	$(".questions").hide();
+
+
+	$(".choose-light").click(function() {
+		button = "light";
+	});
+	$(".choose-dark").click(function() {
+		button = "dark";
+	});	
+
+	$(".start-game").click(function() {
+		if (button === "light") {
+			showGame();
+			increaseCount();
+			//alert("You chose the light side!");
+		} else if (button === "dark") {
+			showGame();
+			//alert("You chose the dark side!");
+		} else {
+			alert("You need to choose a side!");
+		}
+	});
 });
+
+function showGame() {
+	$(".main").hide();
+	$(".questions").show();
+}
+
+function increaseCount() {
+	questionCurrent++;
+	$("#question-current").text(questionCurrent);
+}
